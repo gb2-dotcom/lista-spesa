15f4c8923e3e2ac8d55e53ae70f90420c5b34978
Gabriele <gabriele@Mac.home>
2026-08-01 23:01:58 +0200

    Aggiunge service worker per funzionamento offline

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'lista-spesa-v1';
+const ASSET_DA_CACHARE = [
+  'index.html',
+  'js/app.js',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSET_DA_CACHARE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((nomiCache) =>
+      Promise.all(
+        nomiCache
+          .filter((nome) => nome !== CACHE_NAME)
+          .map((nome) => caches.delete(nome))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((risposta) => risposta || fetch(event.request))
+  );
+});
